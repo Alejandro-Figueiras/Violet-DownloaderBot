d3d4a5aebee75697aeac94bcd40fa134d5382cba
Alejandro <alejandrofigueirasx@gmail.com>
2022-08-14 02:22:11 -0400

    Agregada documentacion y limpieza en los logs

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Violet
-Bot de Telegram para facilitar el manejo de archivos del lado del servidor
+Bot de Telegram para facilitar el manejo de archivos del lado del servidor (indev)
+Y ademas como bono un cliente que descarga archivos de canales restringidos hacia mega
+
+## Cliente
+```bash
+node cliente [--params...]
+```
+### Datos
+Deben especificarse estos 3 primeros parametros para descargar los archivos desde la idstart hasta la idend en el canal o grupo restringido "channel". Estos datos se obtienen de un link:
+https://t.me/channel/24
+
+El out es la ruta de cache de los archivos, si usas heroku puede ser "/tmp/" y esta ultima debe terminar en '/'
+```
+--channel
+--idstart
+--idend
+
+--out
+```
+
+## Environment Variables
+Para el bot:
+```
+BOT_TOKEN="5404716010:AAHA4tjHtsiss3EDtcgYDUP5fAX-abjFfpE"
+```
+
+Para el cliente:
+```
+API_ID=123456
+API_HASH="hash"
+API_TOKEN=""
+
+MEGA_EMAIL="hola@example.com"
+MEGA_PASSWORD="*********"
+```
+
+<blockquote>
+Las API_ID y API_HASH se pueden obtener en <https://my.telegram.org> y el token se obtiene al iniciar sesion la primera vez y se guarda para no tener que hacerlo las veces posteriores
+</blockqoute>
--- clienteMega.js
+++ clienteMega.js
@@ -1,14 +1,3 @@
-/*
-apiid
-apihash
-session
-channel
-id
-offset
-cant
-*/
-
-
 require('dotenv').config();
 
 const yargs = require('yargs/yargs')
@@ -20,15 +9,28 @@ const { StringSession } = require("telegram/sessions");
 const fs = require('fs');
 const input = require("input"); // npm i input
 const { getInputChannel } = require('telegram/Utils');
-
-const prompt = require('prompt');
+const path = require('path');
 
 const apiId = parseInt((argv.apiid)?argv.apiid:process.env.API_ID);
 const apiHash = (argv.apihash)?argv.apihash:process.env.API_HASH;
 const stringSession = new StringSession((argv.session)?argv.session:process.env.API_TOKEN); // fill this later with the value from session.save()
 
+// mega
+const { Storage } = require('megajs')
+
+let storage;
+
+const upload = async(file) => {
+  console.log("Uploading to MEGA:", path.basename(file));
+  await storage.upload({
+    name: path.basename(file),
+    size: fs.statSync(file).size
+  }, fs.createReadStream(file)).complete
+  console.log('The file was uploaded!');
+  return;
+}
+
 (async () => {
-  console.log("Loading interactive example...");
   const client = new TelegramClient(stringSession, apiId, apiHash, {
     connectionRetries: 30,
   });
@@ -39,35 +41,41 @@ const stringSession = new StringSession((argv.session)?argv.session:process.env.
       await input.text("Please enter the code you received: "),
     onError: (err) => console.log(err),
   });
-  console.log("You should now be connected.");
+  console.log("You should now be connected. Save this token for the next time:");
   console.log(client.session.save()); // Save this string to avoid logging in again
   
   // app code functionality
 
+  // mega login
+  storage = await new Storage({
+    email: process.env.MEGA_EMAIL,
+    password: process.env.MEGA_PASSWORD
+  }).ready
+
   const channel = await client.invoke(new Api.channels.GetFullChannel({
     channel: argv.channel
   }));
-
-  let inp = await getInputChannel(channel.chats[0]);
   
+  let inp = await getInputChannel(channel.chats[0]);
+
+  let ids = [];
+  for (let k = argv.idstart; k <= argv.idend; k++) {
+    ids.push(k);
+  }
+
   const msgs = await client.invoke(new Api.channels.GetMessages({
     channel: inp,
-    id: [argv.id]
+    id: ids
   }));
 
-  let offset = argv.offset;
-  let cantidad = argv.cant;
-
   for (const msg of msgs.messages) {
+    console.log(`Downloading ${msg.media.document.attributes[0].fileName} (${msg.media.document.size/(1024*1024)}MB)`);
+
     const chunkSize = 1024*1024;
     const total = Math.ceil(msg.media.document.size / chunkSize)
-
-    let results = [];
-    console.log(`Descargando ${argv.id} offset=${argv.offset} cant=${argv.cant}`);
-    for (let i = offset; i < offset+cantidad; i++) {
+    for (let i = 0; i < total; i++) {
       try {
-        //client.connect();
-        results.push(await client.invoke(new Api.upload.GetFile({
+        const result = await client.invoke(new Api.upload.GetFile({
           location: new Api.InputDocumentFileLocation({
             id: msg.media.document.id,
             accessHash: msg.media.document.accessHash,
@@ -78,32 +86,23 @@ const stringSession = new StringSession((argv.session)?argv.session:process.env.
           limit: chunkSize,
           precise: true,
           cdnSupported: true
-        })));
-        console.log(`Downloaded ${(i+1-offset)/(cantidad)*100}%`);
+        }));
+          if (i==0) {
+            fs.writeFileSync(`${(argv.out)?argv.out:"./out/"}${msg.media.document.attributes[0].fileName}`, result.bytes);
+          } else {
+            fs.appendFileSync(`${(argv.out)?argv.out:"./out/"}${msg.media.document.attributes[0].fileName}`, result.bytes)
+          }
+          console.log(`Downloaded ${(i+1)/total*100}%`);
       } catch (e) {
-        //i--;
+        i--;
         console.error(e);
-        //continue;
-        break;
+        continue;
       }
     }
 
-    prompt.start();
-
-    prompt.get(['Listo para continuar'], async function () {
-      for (let i = 0; i < results.length; i++) {
-        let s = await fs.appendFileSync(`./out/${msg.media.document.attributes[0].fileName}`, results[i].bytes)
-        console.log(s);
-      }
-      console.log("Agregado");
-      process.exit(0)
-    });
+    // upload
+    await upload(`${(argv.out)?argv.out:"./out/"}${msg.media.document.attributes[0].fileName}`);
   }
-  
   //end code 
-  
-    
-
-
 
 })();
