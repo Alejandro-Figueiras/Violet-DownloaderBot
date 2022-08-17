f08eeb21bb444f937682a83d14694d01a27a46f7
Alejandro <alejandrofigueirasx@gmail.com>
2022-08-17 12:07:27 -0400

    Added mega uploading files

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
+
+// NODE MODULES
 const { Telegraf } = require('telegraf')
 const fs = require('fs');
+const path = require('path');
+const download = require('download');
+const { Storage } = require("megajs")
 
+// YARGS
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers');
-const download = require('download');
 const argv = yargs(hideBin(process.argv)).argv
-
-console.log(argv.token | process.env.BOT_TOKEN);
 
 const bot = new Telegraf((argv.token)?argv.token:process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Bienvenido ' + ctx.from.first_name + ", mi nombre es Violet y te ayudaré en el manejo de archivos del lado del servidor"));
@@ -16,6 +19,7 @@ bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.command('oldschool', (ctx) => ctx.reply('Hello'))
 bot.command('hipster', Telegraf.reply('λ'))
+
 
 bot.command("id", async(ctx) => {
     let splitted = ctx.message.text.split(" ");
@@ -60,6 +64,29 @@ bot.on("document", async(ctx) => {
     fs.rmdirSync(process.env.OUT_DIR+random);
     ctx.reply(`Downloaded file: ${document.file_name} ${(document.file_size / (1024 * 1024)).toFixed(2)}MB`);
 })
+
+bot.command("uploadMegaAllDelete", async(ctx) => {
+    // MEGA LOGIN
+    const storage = await new Storage({
+        email: process.env.MEGA_EMAIL,
+        password: process.env.MEGA_PASSWORD
+    }).ready
+
+    // UPLOADING FILES
+    let dir = fs.readdirSync(process.env.OUT_DIR);
+    for (const element of dir) {
+        let file = process.env.OUT_DIR + element;
+        ctx.reply(`Uploading to MEGA: ${path.basename(file)}`);
+        await storage.upload({
+            name: path.basename(file),
+            size: fs.statSync(file).size
+        }, fs.createReadStream(file)).complete
+        ctx.reply('The file was uploaded!');
+        fs.rmSync(file);
+    }
+})
+
+
 
 bot.launch()
 console.log("Iniciando Violet")
